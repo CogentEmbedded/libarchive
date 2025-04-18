20aa10622f81d2f3d5b39a2bef776a5e0d1232b4
Andrey Vostrikov <andrey.vostrikov@cogentembedded.com>
2025-04-18 17:04:39 +0300

    Fix calloc parameters order
    Signed-off-by: Andrey Vostrikov <andrey.vostrikov@cogentembedded.com>

--- libarchive/archive_read_support_filter_uu.c
+++ libarchive/archive_read_support_filter_uu.c
@@ -214,7 +214,7 @@ bid_get_line(struct archive_read_filter *filter,
 {
 	ssize_t len;
 	int quit;
-	
+
 	quit = 0;
 	if (*avail == 0) {
 		*nl = 0;
@@ -374,7 +374,7 @@ uudecode_bidder_init(struct archive_read_filter *self)
 	self->code = ARCHIVE_FILTER_UU;
 	self->name = "uu";
 
-	uudecode = (struct uudecode *)calloc(sizeof(*uudecode), 1);
+	uudecode = (struct uudecode *)calloc(1, sizeof(*uudecode));
 	out_buff = malloc(OUT_BUFF_SIZE);
 	in_buff = malloc(IN_BUFF_SIZE);
 	if (uudecode == NULL || out_buff == NULL || in_buff == NULL) {
